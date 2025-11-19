67a875efc42755faf1e5bf1ea8c6ac458d895f8a
soufian <soufianeboukir0@gmail.com>
2025-11-19 21:15:38 +0100

    setup shadcn

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center w-[100%] mx-auto justify-center bg-zinc-50 font-sans dark:bg-black">
+    <div className="flex min-h-screen items-center mx-auto justify-center bg-zinc-50 font-sans dark:bg-black">
       <h1 className="text-3xl font-semibold">FRONT NEXT.JS</h1>
     </div>
   );
