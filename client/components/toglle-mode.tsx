5f24b93d195a3d8c344cd1d4642c7b9494e7ca02
soufian <soufianeboukir0@gmail.com>
2025-11-20 14:10:32 +0100

    done: sidebar & ask input & theme toggle

---
+++ client/components/toglle-mode.tsx
@@ -0,0 +1,12 @@
+"use client"
+
+import { Sun } from "lucide-react";
+
+export function ToggleMode() {
+    
+  return (
+    <div>
+      <Sun className="w-6 h-6 cursor-pointer"/>
+    </div>
+  );
+}
